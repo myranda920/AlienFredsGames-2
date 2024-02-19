12a41ab0b6ca2877a8c1d4c13a67ad43077c6a93
Myranda Palomino <myranda.palomino@gmail.com>
2024-02-18 20:02:02 -0800

    hover green

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6828dd60e6e5a13"; }
+/******/ 		__webpack_require__.h = function() { return "09889e4c4ddd120d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
